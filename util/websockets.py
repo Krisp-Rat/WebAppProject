64f92821310cce40fd8697797b18058735382d78
Daniel Preisler <32717269+Krisp-Rat@users.noreply.github.com>
2024-11-12 02:05:54 -0500

    Finished LO1 of HW 4

--- util/websockets.py
+++ util/websockets.py
@@ -21,24 +21,23 @@ def parse_ws_frame(input_bytes):
     fin_bit = (input_bytes[0] & 128) >> 7
     opcode = input_bytes[0] & 15
     mask_bit = (input_bytes[1] & 128) >> 7
-    payload_length = input_bytes[1] & 126
+    print("Mask Bit: ", mask_bit)
+    print("Length Bit: ", input_bytes[1])
+    payload_length = input_bytes[1] & 127
     pointer = 2
     if payload_length == 126:
         payload_length = int.from_bytes(input_bytes[2:4])
-        pointer = 4
+        pointer += 2
     elif payload_length == 127:
         payload_length = int.from_bytes(input_bytes[2:10])
-        pointer = 10
-    payload = b'' * payload_length
+        pointer += 8
+    payload = b''
     if mask_bit == 1:
         mask = input_bytes[pointer: pointer + 4]
         pointer += 4
-        extra = payload_length % 4
-        for i in range(0, payload_length - extra, 4):
-            payload = int.from_bytes(input_bytes[pointer + i: pointer + i + 4]) ^ int.from_bytes(mask)
-        if extra != 0:
-            oped = int.from_bytes(input_bytes[payload_length - extra: payload_length]) ^ int.from_bytes(mask[0:extra])
-            payload[payload_length - extra: payload_length] = (oped)
+        for i in range(payload_length):
+            bites = input_bytes[pointer + i] ^ mask[i % 4]
+            payload += bites.to_bytes(1)
 
     else:
         payload = input_bytes[pointer: pointer + payload_length]
@@ -47,24 +46,12 @@ def parse_ws_frame(input_bytes):
 
 
 def generate_ws_frame(input_bytes):
-    frame_bytes = b''
-
+    payload_length = len(input_bytes)
+    if payload_length < 126:
+        length_bit = payload_length.to_bytes()
+    elif payload_length < 65536:
+        length_bit = b'\x7E' + payload_length.to_bytes(length=2)
+    else:
+        length_bit = b'\x7F' + payload_length.to_bytes(length=8)
+    frame_bytes = b'\x81' + length_bit + input_bytes
     return frame_bytes
-
-
-mask_bites = b'\xa1\xb2\xc3\xd4'
-pre_payload = b'\xf9\xe8\xd7\xc6\xb5\xa4'
-
-
-input_bytes = b'\x02\x06\x10\x10\xe1\xa3\x13\x04'
-frame = parse_ws_frame(input_bytes)
-if frame.payload_length != 6:
-    print("Invalid payload length")
-if frame.opcode != 2:
-    print("Invalid opcode: ", frame.opcode)
-if frame.fin_bit != 0:
-    print("Invalid fin_bit", frame.fin_bit)
-if frame.payload_length != len(frame.payload):
-    print("Invalid payload")
-
-print(frame.payload)
